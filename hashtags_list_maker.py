263d143041b9a4fecf4dab466f6ca6e4be40d2cd
Stella Choi <chsweet@umich.edu>
2017-10-04 11:27:30 -0400

    add TODO Parsing using BeautifulSoup

--- hashtags_list_maker.py
+++ hashtags_list_maker.py
@@ -1,6 +1,8 @@
 import logging, sys
 import re
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 class ListMaker(object):
     def top_hashtag1(self, file_name, output_name):
@@ -12,13 +14,26 @@ class ListMaker(object):
         pattern = re.compile(r"title=.+?\">(.+?)</a>")
         self.__create_list(pattern, file_name, output_name)
    
+    # TODO: parse using Beautiful Soup
+    def top_slangs2(self, file_name, output_nam):
+        try:
+            slangs_data = open("slangs1.html", 'r').text
+        except:
+            slangs_data = requests.get("https://en.wiktionary.org/wiki/Appendix:English_internet_slang").text
+            f = open("slangs1.thml" , 'w')
+            f.write(slangs_data)
+            f.close
+
+        soup = BeautifulSoup(slangs_data, 'html.parser')
+        div = soup.find(id='mw-content-text')
+
     def __create_list(self, pattern, file_name, output_name):
         with open(file_name, 'r') as file:
             content = file.read()
             list = re.findall(pattern, content)
             logging.debug(list)
             df = pd.DataFrame(list, columns=['hashtags/slangs'])
-            df.to_csv(output_name, index=False)                
+            df.to_csv(output_name, index=False)
         
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
