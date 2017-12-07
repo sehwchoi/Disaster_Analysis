4cce89be5c15a2accc5cffc59a2c55a7450f7469
Stella Choi <chsweet@umich.edu>
2017-12-07 12:28:28 -0500

    add comments

--- text_process/tweets_text_processor.py
+++ text_process/tweets_text_processor.py
@@ -11,7 +11,6 @@ import smart_open
 import json
 import csv
 import time
-from word_count_model import WordsDBManager
 sys.path.append('../')
 from meta_data_helper import EventMetaDataHelper
 
@@ -19,6 +18,18 @@ class TextProcessor(object):
     geotag_folder = "geotagged_from_archive"
     timeline_folder = "user_timelines"
 
+    """ Reads all files for all events and counts word, hashtag, emojis frequencies by date for each event
+
+        Attributes:
+            self.output_path (string) : output path where user stats should be saved
+            self.input_paths (list) : input paths of geotag files and timeline files
+            self.event_helper (EventMetaDataHelper class) :
+            self.events (list) : Ex) [1, 2, 3....]
+
+            self.total_tweets (int) :
+            self.unique_id_set (list) : tracks the unique tweet id in geotagged files
+            self.word_counts_by_date (dict) : {word(str) : {date(str): count(int), date(str): count...
+    """
     def __init__(self, incident_metadata_path, input_paths, output_path):
 
         self.input_paths = input_paths
@@ -29,14 +40,14 @@ class TextProcessor(object):
         self.events = self.event_helper.get_all_events()
         logging.debug(self.events)
 
+        # download nltk libraries
         nltk.download('punkt')
         nltk.download('stopwords')
-        # stop words
+        # get stop words
         self.stop_words = set(stopwords.words('english'))
         # logging.debug("Stop words : {}".format(self.stop_words))
-        # get words
-        # self.tokenizer = RegexpTokenizer(r'\w+')
 
+        # tracking data for each event
         self.total_tweets = 0
         self.unique_id_set = []
         self.words_counts_by_date = {}
@@ -48,8 +59,7 @@ class TextProcessor(object):
         self.tweets_counts_by_date = {}
 
     def __get_files(self, event_id):
-        """ parse all tweets files all events from the given folder"""
-        # { event1: [files], event2: [files }
+        """ parse all geotagged/timeline files for given event_id"""
         geotag_files = []
         timeline_files = []
         for folder in self.input_paths:
@@ -71,9 +81,8 @@ class TextProcessor(object):
         return geotag_files, timeline_files
 
     def __tokenize_tweet(self, tweet):
-        # remove users, hashtags, any links
-        # punctuation = ",./<>?;':\"[]\\{}|`~!@#$%^&*()_+-="
-        # tweet = re.sub(r"(@[a-zA-Z0-9_]+)|(\w+:\/\/\S+)|([“”,.…/<>;':\"\[\]\\{}|'~?!@#$%^&*()_+-=]+)", " ", tweet)
+        """ tokenize tweet text into word, hashtag word, and emojis all"""
+        # remove users and any links
         tweet = re.sub(r"(@[a-zA-Z0-9_]+)|(\w+:\/\/\S+)", " ", tweet)
         # logging.debug(tweet)
 
@@ -83,15 +92,17 @@ class TextProcessor(object):
         words_filtered = []
         for w in words:
             w = w.lower()
-            # include emoji, alphanumeric, and hashtag strings
+            # include str only if emoji, alphanumeric, and hashtag strings
             if (w in UNICODE_EMOJI) or (w.isalnum()) or (w.startswith("#")):
+                # filter out any stopword and a word already added
                 if (w not in self.stop_words) and (w not in words_filtered):
                     words_filtered.append(w)
         return words_filtered
 
     def __update_count(self, words, date):
-        # update word and count
+        """ add or update word frequencies"""
         for w in words:
+            # if word already added, then increment frequencies
             if w in self.words_counts_by_date:
                 info = self.words_counts_by_date[w]
                 if date in info:
@@ -100,6 +111,7 @@ class TextProcessor(object):
                 else:
                     # logging.debug("word data not exist in the container")
                     self.words_counts_by_date[w][date] = 1
+            # if word not added before, then add it with a proper date
             else:
                 # logging.debug("word not exist in the container")
                 info = {date: 1}
@@ -107,9 +119,9 @@ class TextProcessor(object):
             # logging.debug(self.words_counts_by_date)
 
     def __read_tweets(self, file_name_full, type, event):
+        """ read tweets from file and count word frequencies as it reads"""
         with smart_open.smart_open(file_name_full, 'r') as f:
             logging.debug("file: {}".format(file_name_full))
-            """ read tweets from files and classify them as it reads"""
             for line in f:
                 try:
                     # do not add duplicated tweet by tweet unique id
@@ -122,6 +134,7 @@ class TextProcessor(object):
                         tweet_date_utc = tweet['created_at']
                         tweet_date_local = self.event_helper.convert_to_loctime_from_event(tweet_date_utc,
                                                                                            event)
+                        # only use year, month, and date
                         date = str(tweet_date_local.date())
                         # logging.debug("tweet: {} date: {}".format(tweet_text, date))
                         words = self.__tokenize_tweet(tweet_text)
@@ -129,19 +142,21 @@ class TextProcessor(object):
                         self.__update_count(words, date)
                         self.total_tweets += 1
                         # update tweet id if from geotag file
+                        # if a tweet is from geotag add it to unique tweet set to filter out any duplication later when
+                        # reading timeline
                         if type == "geotag":
                             self.unique_id_set.append(tweet_id)
                 except Exception as e:
                     logging.debug("could not read tweet error: {}".format(e))
 
     def process_tweets(self):
+        """ read tweets from files and count word frequencies for for each event"""
         for event in self.events:
             (geotag_files, timeline_files) = self.__get_files(int(event))
 
             # classify user tweets only if files exist in both geotagged and timeline
             #if event in self.geotaged_exist and event in self.timeline_exist and not os.path.isfile(output):
-            #if (len(geotag_files) > 0) and (len(timeline_files) > 0):
-            if (len(geotag_files) > 0) or (len(timeline_files) > 0):
+            if (len(geotag_files) > 0) and (len(timeline_files) > 0):
                 for file_name in geotag_files:
                     file_name_full = os.path.join(self.input_paths[0], file_name)
                     logging.debug("start reading file {}".format(file_name_full))
@@ -155,7 +170,7 @@ class TextProcessor(object):
                 self.__reset_event_data()
 
     def __save_stats(self, output):
-        """ create information of each date and number of tweets
+        """ create information of (word, date, count)
             save all information to json file ex. "319_user_stats.csv"""
         logging.debug("save file: {} total tweets: {} ".format(output, self.total_tweets))
         # remove file if it exist
@@ -182,6 +197,5 @@ if __name__ == '__main__':
     output_path = "../../words_count"
     incident_metadata_path = '../incident_metadata.csv'
     text_processor = TextProcessor(incident_metadata_path, input_paths, output_path)
-    #event = 319
     text_processor.process_tweets()
-    # Word, Date, Number of tweets that had the word
+
